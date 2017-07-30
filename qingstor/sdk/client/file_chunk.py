f41bb599db42fa0156540c534473ab5ec5a47ffe
sightseer <xuynktw@gmail.com>
2017-07-30 15:56:01 +0800

    build a FileChunk class, all file operation related to multipart upload included, paarameters and attributes comment added, the feature of printing status message in uploading added

---
+++ qingstor/sdk/client/file_chunk.py
@@ -0,0 +1,51 @@
+import os
+class FileChunk:
+    '''file processing,including open file,move file pointer and read file
+
+    Parameter:
+        file_path(str):the relative path of the file
+        part_size(int): the part size users want to partition of the file in byte
+
+    Attributes:
+        file_path(str):the relative path of the file
+        part_size(int): the part size users want to partition of the file in byte
+        source_file:source_file refers to the uploading file object
+        part_num_list(int): the list of part index which hasn't yet uploaded 
+        cur_read_part(str): the string of current part reading from the source file
+        part_amount(int): the total part number partitioned of the source file according to the part size
+
+    '''
+    def __init__(self,file_path,part_size):
+        self.file_path=file_path
+        self.part_num_list=[]       
+        self.cur_read_part=""
+        self.part_size=part_size
+
+    def part_num_list_init(self):
+        part_amount=self.get_file_part_amount()
+        #initialize the list, part index from 0 to part_amount-1
+        for i in range(0,part_amount):
+            self.part_num_list+=[i]
+
+    #the method is to calculate the total part number
+    def get_file_part_amount(self):
+        part_size=self.part_size
+        file_path=self.file_path    
+        part_amount=os.path.getsize(file_path)/part_size
+        last_part=os.path.getsize(file_path)-part_size*part_amount 
+        if  last_part>0:
+             part_amount+=1
+        return part_amount
+
+    def read_file_part(self,part_index):
+        part_size=self.part_size
+        with open(self.file_path,"rb") as source_file:
+            #move the original file pointer to the part_index
+            source_file.seek(part_size*part_index,0)
+            #read the current part content returned in the cur_read_part		
+    	    cur_read_part=source_file.read(part_size)	
+        return cur_read_part
+    
+
+
+
---
+++ qingstor/sdk/client/uploadClient.py
@@ -0,0 +1,69 @@
+from qingstor.sdk.service.qingstor import QingStor
+from file_chunk import FileChunk
+import os
+class UploadClient:
+    '''file processing,including open file,move file pointer and read file
+
+    Parameter:
+        bucket: bucket refers to bucket object users creating in the QingCloud
+        part_size_MB(int):the part size users want to partition of the file in MB
+        file_name: the key(usually using file name) of the uploading file
+        file_path(str): the relative path of the file
+        
+
+    Attributes:
+        bucket: bucket refers to bucket object users creating in the QingCloud
+        part_size_MB(int): the part size users want to partition of the file in MB
+        file_name: the key(usually using file name) of the uploading file
+        file_path(str): the relative path of the file
+        part_size(int): the part size users want to partition of the file in byte
+        source_file: source_file refers to the uploading file object (from class FileChunk)
+        part_num_list(int): the list of part index which hasn't yet uploaded (from class FileChunk)
+        cur_read_part(str): the string of current part reading from the source file (from class FileChunk)        
+        part_amount(int): the total part number partitioned of the source file according to the part size (from class FileChunk)
+        this_upload_id: the unique upload id of the uploading file
+        file_chunk: the instance of the class FileChunk
+        part_index: the index of each part
+        part_list: saving the part index which completes uploading
+        
+    
+    '''
+    #httpstatus code
+    status_part_created=201
+    status_ok=200
+    status_bad_request=400
+
+    def __init__(self,bucket,part_size_MB):
+    	self.bucket=bucket
+        self.part_size=part_size_MB*1024*1024
+        
+    def upload_large_file(self,file_name,file_path):
+        #initiate multipart upload, create an upload id
+        output=self.bucket.initiate_multipart_upload(file_name) 
+        if output.status_code==self.status_ok:
+            print("Multipart Upload Initialization Done!")
+        else:
+            print("Invalid Request!") 
+        this_upload_id=output['upload_id']
+        file_chunk=FileChunk(file_path,self.part_size)
+        file_chunk.part_num_list_init()
+        #part_list stores the part number which completes uploading
+        part_list=[]                
+        while len(file_chunk.part_num_list):
+            part_index=file_chunk.part_num_list[0]
+            cur_read_part=file_chunk.read_file_part(part_index)
+            output=self.bucket.upload_multipart(file_name,upload_id=this_upload_id,part_number=part_index,body=cur_read_part)
+            if output.status_code==self.status_part_created:
+                part_list+=[{"part_number":part_index}]
+                file_chunk.part_num_list.pop(0)
+                print("%d Part Uploaded!"%part_index)
+            elif output.status_code==self.status_bad_request:
+                print("Part Uploading Error!Error Cases:\n[entity_too_small]The part size of the uploading file is smaller than the smallest limitation(4M)\n[max_parts_exceeded]: The number of part exceeds part limitation(1000)\n[upload_not_exists]: Upload id does not exist")
+        if len(file_chunk.part_num_list)==0:
+            complete_output=self.bucket.complete_multipart_upload(file_name,this_upload_id,object_parts=part_list) 
+            print(complete_output.content)
+            return complete_output 
+
+
+
+
