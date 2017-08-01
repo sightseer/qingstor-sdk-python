a872ca3fca06f4db93ce183e70a4d4ff510816ca
sightseer <xuynktw@gmail.com>
2017-08-01 17:21:59 +0800

    Method merged, exception added Part_wait_list init method merged into __init__, Part size too small exception added

--- qingstor/sdk/client/file_chunk.py
+++ qingstor/sdk/client/file_chunk.py
@@ -1,51 +1,54 @@
 import os
+from ..error import PartTooSmallError
 class FileChunk:
     '''file processing,including open file,move file pointer and read file
 
     Parameter:
-        file_path(str):the relative path of the file
         part_size(int): the part size users want to partition of the file in byte
+        part_wait_list(int): the list of part index which is waiting to upload 
+        cur_read_part(str): the string of current part reading from the source file
+        file_descriptor(object): the source uploading file object
+        part_amount(int): the total part number partitioned of the source file according to the part size
 
     Attributes:
-        file_path(str):the relative path of the file
         part_size(int): the part size users want to partition of the file in byte
-        source_file:source_file refers to the uploading file object
         part_wait_list(int): the list of part index which is waiting to upload 
         cur_read_part(str): the string of current part reading from the source file
         part_amount(int): the total part number partitioned of the source file according to the part size
+        part_index: the index of each part
 
     '''
-    def __init__(self,file_path,part_size):
-        self.file_path=file_path
-        self.part_wait_list=[]       
+    def __init__(self,part_size,file_descriptor):
+        if part_size<4:
+            raise PartTooSmallError()
+        self.part_wait_list=[]     
         self.cur_read_part=""
         self.part_size=part_size
-
-    def part_wait_list_init(self):
-        part_amount=self.get_file_part_amount()
-        #initialize the list, part index from 0 to part_amount-1
-        for i in range(0,part_amount):
+        self.file_descriptor=file_descriptor
+        self.part_amount=self.get_file_part_amount()
+        #initialize the part_wait_list according to part_amount
+        for i in range(0,self.part_amount):
             self.part_wait_list+=[i]
 
     #the method is to calculate the total part number
     def get_file_part_amount(self):
+        self.file_descriptor.seek(0,2)
+        file_size=self.file_descriptor.tell()
         part_size=self.part_size
-        file_path=self.file_path    
-        part_amount=os.path.getsize(file_path)/part_size
-        last_part=os.path.getsize(file_path)-part_size*part_amount 
+        part_amount=file_size/part_size
+        last_part=file_size-part_size*part_amount 
         if  last_part>0:
              part_amount+=1
         return part_amount
 
     def read_file_part(self,part_index):
         part_size=self.part_size
-        with open(self.file_path,"rb") as source_file:
-            #move the original file pointer to the part_index
-            source_file.seek(part_size*part_index,0)
-            #read the current part content returned in the cur_read_part		
-    	    cur_read_part=source_file.read(part_size)	
+        #move the original file pointer to the part_index
+        self.file_descriptor.seek(part_size*part_index,0)
+        #read the current part content returned in the cur_read_part		
+    	cur_read_part=self.file_descriptor.read(part_size)	
         return cur_read_part
-    
+
 
 
 
