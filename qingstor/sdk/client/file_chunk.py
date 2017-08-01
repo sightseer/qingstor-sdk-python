42b03aa9c3fa933f3b80cb7998c6ce389e201ac0
sightseer <xuynktw@gmail.com>
2017-08-01 21:55:08 +0800

    Some code style changed

--- qingstor/sdk/client/file_chunk.py
+++ qingstor/sdk/client/file_chunk.py
@@ -1,23 +1,22 @@
 import os
-from ..error import PartTooSmallError
+
+from error import PartTooSmallError
+
+
 class FileChunk:
     '''file processing,including open file,move file pointer and read file
 
     Parameter:
         part_size(int): the part size users want to partition of the file in byte
-        part_wait_list(int): the list of part index which is waiting to upload 
-        cur_read_part(str): the string of current part reading from the source file
         file_descriptor(object): the source uploading file object
-        part_amount(int): the total part number partitioned of the source file according to the part size
 
     Attributes:
         part_size(int): the part size users want to partition of the file in byte
-        part_wait_list(int): the list of part index which is waiting to upload 
         cur_read_part(str): the string of current part reading from the source file
-        part_amount(int): the total part number partitioned of the source file according to the part size
         part_index: the index of each part
 
     '''
+
     def __init__(self,part_size,file_descriptor):
         if part_size<4:
             raise PartTooSmallError()
@@ -26,13 +25,13 @@ class FileChunk:
         self.part_size=part_size
         self.file_descriptor=file_descriptor
         self.part_amount=self.get_file_part_amount()
-        #initialize the part_wait_list according to part_amount
+        # Initialize the part_wait_list according to part_amount.
         for i in range(0,self.part_amount):
             self.part_wait_list+=[i]
 
-    #the method is to calculate the total part number
+    # The method is to calculate the total part number.
     def get_file_part_amount(self):
-        self.file_descriptor.seek(0,2)
+        self.file_descriptor.seek(0,os.SEEK_END)
         file_size=self.file_descriptor.tell()
         part_size=self.part_size
         part_amount=file_size/part_size
@@ -43,9 +42,9 @@ class FileChunk:
 
     def read_file_part(self,part_index):
         part_size=self.part_size
-        #move the original file pointer to the part_index
-        self.file_descriptor.seek(part_size*part_index,0)
-        #read the current part content returned in the cur_read_part		
+        # Move the original file pointer to the part_index.
+        self.file_descriptor.seek(part_size*part_index,os.SEEK_SET)
+        # Read the current part content returned in the cur_read_part.		
     	cur_read_part=self.file_descriptor.read(part_size)	
         return cur_read_part
 
