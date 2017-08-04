883c42ce39706822d66eb89bf7713ff739482e9d
sightseer <xuynktw@gmail.com>
2017-08-04 12:01:09 +0800

    New test method added The method read_part_file replaced by read, testing method for get_file_amount added

--- tests/client/test_file_chunk.py
+++ tests/client/test_file_chunk.py
@@ -1,23 +1,32 @@
+import os
 import unittest
 from read_file_part import *
 from qingstor.sdk.client.file_chunk import FileChunk
 
 
 # The part size of each reading
-PART_SIZE = 50
-PART_INDEX = 18
-FILE_PATH="test_file_12M.mp3"
+TEST_PART_SIZE = 50
+TEST_PART_INDEX = 10
+#expected_amount = size(test_small_file.txt)/test_part_size=896/50=18
+EXPECTED_AMOUNT = 18
+TEST_FILE_PATH="test_small_file.txt"
 
 class TestFileChunk(unittest.TestCase):
     def test_read_file_part(self):
-        expected_output=""
-        with open(FILE_PATH, "rb") as f:
-            expected_output=read_file_part(PART_INDEX,f)
-        test_output=""
-        with open(FILE_PATH, "rb") as f:
-            temp_file_chunk = FileChunk(PART_SIZE, f)
-            test_output = temp_file_chunk.read_file_part(PART_INDEX)
+        with open(TEST_FILE_PATH, "rb") as f:
+            f.seek(TEST_PART_SIZE*TEST_PART_INDEX, os.SEEK_SET)
+            expected_output=read_file_part(TEST_PART_INDEX,f)
+            f.seek(0, os.SEEK_SET)
+            temp_file_chunk = FileChunk(TEST_PART_SIZE, f)
+            test_output = temp_file_chunk.read_file_part(TEST_PART_INDEX)
         self.assertEqual(expected_output,test_output)
+
+    def test_get_part_amount(self):
+      #  test_amount=0
+        with open(TEST_FILE_PATH, "rb") as f:
+            temp_file_chunk = FileChunk(TEST_PART_SIZE, f)
+            test_amount=temp_file_chunk.get_file_part_amount()
+        self.assertEqual(test_amount,EXPECTED_AMOUNT)
 
 if __name__=="__main__":
     unittest.main()
